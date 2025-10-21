d5cb16727d71b7d94f97bef00a6eeaf9fd450f57
Sérgio Luiz Moral Marques <sergio@academico.cotuca.unicamp.br>
2025-10-21 10:49:01 -0300

    Ajust no yaml 4

--- jest.config.js
+++ jest.config.js
@@ -2,3 +2,8 @@
 {
   reporters: [ "default", "jest-junit" ]
 }
+
+//module.exports = config;
+module.exports = {
+      reporters: ['default', ['jest-junit', { outputDirectory: 'reports', outputName: 'junit.xml' }]],
+    };
